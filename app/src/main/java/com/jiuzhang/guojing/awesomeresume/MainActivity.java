025fc7e096c38aefb9fafd60a8a27984be717da7
Yangfei Li <lyfelmer@outlook.com>
2017-12-27 22:41:47 -0500

    app

---
+++ app/src/main/java/com/jiuzhang/guojing/awesomeresume/MainActivity.java
@@ -0,0 +1,284 @@
+package com.jiuzhang.guojing.awesomeresume;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.v7.app.AppCompatActivity;
+import android.text.TextUtils;
+import android.view.View;
+import android.widget.ImageButton;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+
+import com.google.gson.reflect.TypeToken;
+import com.jiuzhang.guojing.awesomeresume.model.BasicInfo;
+import com.jiuzhang.guojing.awesomeresume.model.Education;
+import com.jiuzhang.guojing.awesomeresume.model.Project;
+import com.jiuzhang.guojing.awesomeresume.util.DateUtils;
+import com.jiuzhang.guojing.awesomeresume.util.ImageUtils;
+import com.jiuzhang.guojing.awesomeresume.util.ModelUtils;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@SuppressWarnings("ConstantConditions")
+public class MainActivity extends AppCompatActivity {
+
+    private static final int REQ_CODE_EDIT_EDUCATION = 100;
+    private static final int REQ_CODE_EDIT_PROJECT = 102;
+    private static final int REQ_CODE_EDIT_BASIC_INFO = 103;
+
+    private static final String MODEL_EDUCATIONS = "educations";
+    private static final String MODEL_PROJECTS = "projects";
+    private static final String MODEL_BASIC_INFO = "basic_info";
+
+    private BasicInfo basicInfo;
+    private List<Education> educations;
+    private List<Project> projects;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        loadData();
+        setupUI();
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (resultCode == Activity.RESULT_OK) {
+            switch (requestCode) {
+                case REQ_CODE_EDIT_BASIC_INFO:
+                    BasicInfo basicInfo = data.getParcelableExtra(BasicInfoEditActivity.KEY_BASIC_INFO);
+                    updateBasicInfo(basicInfo);
+                    break;
+                case REQ_CODE_EDIT_EDUCATION:
+                    String educationId = data.getStringExtra(EducationEditActivity.KEY_EDUCATION_ID);
+                    if (educationId != null) {
+                        deleteEducation(educationId);
+                    } else {
+                        Education education = data.getParcelableExtra(EducationEditActivity.KEY_EDUCATION);
+                        updateEducation(education);
+                    }
+                    break;
+                case REQ_CODE_EDIT_PROJECT:
+                    String projectId = data.getStringExtra(ProjectEditActivity.KEY_PROJECT_ID);
+                    if (projectId != null) {
+                        deleteProject(projectId);
+                    } else {
+                        Project project = data.getParcelableExtra(ProjectEditActivity.KEY_PROJECT);
+                        updateProject(project);
+                    }
+                    break;
+            }
+        }
+    }
+
+    private void setupUI() {
+        setContentView(R.layout.activity_main);
+
+        ImageButton addEducationBtn = (ImageButton) findViewById(R.id.add_education_btn);
+        addEducationBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, EducationEditActivity.class);
+                startActivityForResult(intent, REQ_CODE_EDIT_EDUCATION);
+            }
+        });
+
+        ImageButton addProjectBtn = (ImageButton) findViewById(R.id.add_project_btn);
+        addProjectBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, ProjectEditActivity.class);
+                startActivityForResult(intent, REQ_CODE_EDIT_PROJECT);
+            }
+        });
+
+        setupBasicInfo();
+        setupEducations();
+        setupProjects();
+    }
+
+    private void setupBasicInfo() {
+        ((TextView) findViewById(R.id.name)).setText(TextUtils.isEmpty(basicInfo.name)
+                                                             ? "Your name"
+                                                             : basicInfo.name);
+        ((TextView) findViewById(R.id.email)).setText(TextUtils.isEmpty(basicInfo.email)
+                                                             ? "Your email"
+                                                             : basicInfo.email);
+
+        ImageView userPicture = (ImageView) findViewById(R.id.user_picture);
+        if (basicInfo.imageUri != null) {
+            ImageUtils.loadImage(this, basicInfo.imageUri, userPicture);
+        } else {
+            userPicture.setImageResource(R.drawable.user_ghost);
+        }
+
+        findViewById(R.id.edit_basic_info).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, BasicInfoEditActivity.class);
+                intent.putExtra(BasicInfoEditActivity.KEY_BASIC_INFO, basicInfo);
+                startActivityForResult(intent, REQ_CODE_EDIT_BASIC_INFO);
+            }
+        });
+    }
+
+    private void setupEducations() {
+        LinearLayout educationsLayout = (LinearLayout) findViewById(R.id.education_list);
+        educationsLayout.removeAllViews();
+        for (Education education : educations) {
+            View educationView = getLayoutInflater().inflate(R.layout.education_item, null);
+            setupEducation(educationView, education);
+            educationsLayout.addView(educationView);
+        }
+    }
+
+    private void setupEducation(View educationView, final Education education) {
+        String dateString = DateUtils.dateToString(education.startDate)
+                + " ~ " + DateUtils.dateToString(education.endDate);
+        ((TextView) educationView.findViewById(R.id.education_school))
+                .setText(education.school + " " + education.major + " (" + dateString + ")");
+        ((TextView) educationView.findViewById(R.id.education_courses))
+                .setText(formatItems(education.courses));
+
+        ImageButton editEducationBtn = (ImageButton) educationView.findViewById(R.id.edit_education_btn);
+        editEducationBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, EducationEditActivity.class);
+                intent.putExtra(EducationEditActivity.KEY_EDUCATION, education);
+                startActivityForResult(intent, REQ_CODE_EDIT_EDUCATION);
+            }
+        });
+    }
+
+    private void setupProjects() {
+        LinearLayout projectListLayout = (LinearLayout) findViewById(R.id.project_list);
+        projectListLayout.removeAllViews();
+        for (Project project : projects) {
+            View projectView = getLayoutInflater().inflate(R.layout.project_item, null);
+            setupProject(projectView, project);
+            projectListLayout.addView(projectView);
+        }
+    }
+
+    private void setupProject(@NonNull View projectView, final Project project) {
+        String dateString = DateUtils.dateToString(project.startDate)
+                + " ~ " + DateUtils.dateToString(project.endDate);
+        ((TextView) projectView.findViewById(R.id.project_name))
+                .setText(project.name + " (" + dateString + ")");
+        ((TextView) projectView.findViewById(R.id.project_details))
+                .setText(formatItems(project.details));
+        projectView.findViewById(R.id.edit_project_btn).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, ProjectEditActivity.class);
+                intent.putExtra(ProjectEditActivity.KEY_PROJECT, project);
+                startActivityForResult(intent, REQ_CODE_EDIT_PROJECT);
+            }
+        });
+    }
+
+    private void loadData() {
+        BasicInfo savedBasicInfo = ModelUtils.read(this,
+                                                   MODEL_BASIC_INFO,
+                                                   new TypeToken<BasicInfo>(){});
+        basicInfo = savedBasicInfo == null ? new BasicInfo() : savedBasicInfo;
+
+        List<Education> savedEducation = ModelUtils.read(this,
+                                                         MODEL_EDUCATIONS,
+                                                         new TypeToken<List<Education>>(){});
+        educations = savedEducation == null ? new ArrayList<Education>() : savedEducation;
+
+        List<Project> savedProjects = ModelUtils.read(this,
+                                                     MODEL_PROJECTS,
+                                                     new TypeToken<List<Project>>(){});
+        projects = savedProjects == null ? new ArrayList<Project>() : savedProjects;
+    }
+
+    public static String formatItems(List<String> items) {
+        StringBuilder sb = new StringBuilder();
+        for (String item: items) {
+            sb.append(' ').append('-').append(' ').append(item).append('\n');
+        }
+        if (sb.length() > 0) {
+            sb.deleteCharAt(sb.length() - 1);
+        }
+        return sb.toString();
+    }
+
+    private void updateBasicInfo(BasicInfo basicInfo) {
+        ModelUtils.save(this, MODEL_BASIC_INFO, basicInfo);
+
+        this.basicInfo = basicInfo;
+        setupBasicInfo();
+    }
+
+    private void updateEducation(Education education) {
+        boolean found = false;
+        for (int i = 0; i < educations.size(); ++i) {
+            Education e = educations.get(i);
+            if (TextUtils.equals(e.id, education.id)) {
+                found = true;
+                educations.set(i, education);
+                break;
+            }
+        }
+
+        if (!found) {
+            educations.add(education);
+        }
+
+        ModelUtils.save(this, MODEL_EDUCATIONS, educations);
+        setupEducations();
+    }
+
+    private void updateProject(Project project) {
+        boolean found = false;
+        for (int i = 0; i < projects.size(); ++i) {
+            Project p = projects.get(i);
+            if (TextUtils.equals(p.id, project.id)) {
+                found = true;
+                projects.set(i, project);
+                break;
+            }
+        }
+
+        if (!found) {
+            projects.add(project);
+        }
+
+        ModelUtils.save(this, MODEL_PROJECTS, projects);
+        setupProjects();
+    }
+
+    private void deleteEducation(@NonNull String educationId) {
+        for (int i = 0; i < educations.size(); ++i) {
+            Education e = educations.get(i);
+            if (TextUtils.equals(e.id, educationId)) {
+                educations.remove(i);
+                break;
+            }
+        }
+
+        ModelUtils.save(this, MODEL_EDUCATIONS, educations);
+        setupEducations();
+    }
+
+    private void deleteProject(@NonNull String projectId) {
+        for (int i = 0; i < projects.size(); ++i) {
+            Project p = projects.get(i);
+            if (TextUtils.equals(p.id, projectId)) {
+                projects.remove(i);
+                break;
+            }
+        }
+
+        ModelUtils.save(this, MODEL_PROJECTS, projects);
+        setupProjects();
+    }
+}
